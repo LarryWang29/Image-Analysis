d34f5334483530007a1d0e55debb8905fde8bf35
Larry Wang <dw661@cam.ac.uk>
2024-06-14 22:36:19 +0100

    DEV: Added script for segmenting the flowers picture

---
+++ Q1-flowers.py
@@ -0,0 +1,98 @@
+import skimage
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.restoration import denoise_tv_chambolle
+from skimage.morphology import binary_closing, binary_opening, disk
+from skimage.color import rgb2lab
+from sklearn.cluster import KMeans
+
+
+def find_purple_cluster(kmeans_cluster):
+    purple_lab = np.array([45.36, 78.75, -77.41])
+    distances = np.sum((kmeans_cluster.cluster_centers_ - purple_lab) ** 2,
+                       axis=1)
+
+    # Find the cluster closest to the purple lab color
+    purple_cluster = np.argmin(distances)
+    return purple_cluster
+
+
+# Read the images
+flowers = skimage.io.imread('data/noisy_flower.jpg')[:, :, :3]
+flowers_gray = skimage.io.imread('data/noisy_flower.jpg', as_gray=True)
+
+# Denoise using Total Variation filter
+flowers_denoised = denoise_tv_chambolle(flowers, weight=0.1)
+
+# Plot both the originial and the denoised images
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.imshow(flowers)
+
+# Hide the axes
+plt.axis('off')
+plt.title('Original noisy image')
+
+plt.subplot(1, 2, 2)
+plt.imshow(flowers_denoised)
+
+# Hide the axes
+plt.axis('off')
+plt.title('Denoised image')
+plt.tight_layout()
+plt.savefig('figures/denoised_comparison.png')
+
+flowers_lab = rgb2lab(flowers_denoised)
+flowers_noisy_lab = rgb2lab(flowers)
+
+kmeans_denoised = KMeans(n_clusters=5,
+                         random_state=0,
+                         n_init=25).fit(flowers_lab.reshape(-1, 3))
+
+kmeans_noisy = KMeans(n_clusters=5,
+                      random_state=0,
+                      n_init=25).fit(flowers_noisy_lab.reshape(-1, 3))
+
+# Find the cluster closest to the purple lab color
+purple_cluster_denoised = find_purple_cluster(kmeans_denoised)
+purple_cluster_noisy = find_purple_cluster(kmeans_noisy)
+
+denoised_labels = kmeans_denoised.labels_.reshape(666, 1000)
+noisy_labels = kmeans_noisy.labels_.reshape(666, 1000)
+
+# Plot the noisy and denoised images with the purple cluster highlighted
+plt.figure(figsize=(10, 4))
+plt.subplot(1, 2, 1)
+plt.imshow(noisy_labels == purple_cluster_noisy, cmap='gray')
+
+# Hide the axes
+plt.axis('off')
+plt.title('Purple cluster in noisy image')
+
+plt.subplot(1, 2, 2)
+plt.imshow(denoised_labels == purple_cluster_denoised, cmap='gray')
+
+# Hide the axes
+plt.axis('off')
+plt.title('Purple cluster in denoised image')
+plt.tight_layout()
+plt.savefig('figures/purple_cluster.png')
+
+# Further denoise the cluster by performing binary closing followed by
+# binary opening
+purple_mask_closed = binary_closing(denoised_labels == 2,
+                                    disk(1))
+plt.figure(figsize=(5, 4))
+plt.imshow(purple_mask_closed, cmap='gray')
+plt.axis('off')
+plt.title('Purple cluster after binary closing')
+plt.tight_layout()
+plt.savefig('figures/closed_purple_mask.png')
+
+purple_mask_opened = binary_opening(purple_mask_closed, disk(2))
+plt.figure(figsize=(5, 4))
+plt.imshow(purple_mask_opened, cmap='gray')
+plt.axis('off')
+plt.title('Purple cluster after binary closing and opening')
+plt.tight_layout()
+plt.savefig('figures/opened_purple_mask.png')
